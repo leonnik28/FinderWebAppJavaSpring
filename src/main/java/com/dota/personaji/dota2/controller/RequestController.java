f766640a0ce1ca3a087a64327d527e2f13eb9f5e
leonnik28 <lencruseckij@gmail.com>
2024-04-25 09:58:24 +0300

    CodeSmell

--- src/main/java/com/dota/personaji/dota2/controller/RequestController.java
+++ src/main/java/com/dota/personaji/dota2/controller/RequestController.java
@@ -3,7 +3,6 @@ package com.dota.personaji.dota2.controller;
 import com.dota.personaji.dota2.service.RequestCounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
