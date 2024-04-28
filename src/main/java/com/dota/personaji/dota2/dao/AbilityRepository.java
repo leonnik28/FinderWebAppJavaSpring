b0ce914d607823d0b6fb6d84ee44fdd634f2cf9a
leonnik28 <lencruseckij@gmail.com>
2024-04-29 01:02:32 +0300

    RemoveCodeSmells

--- src/main/java/com/dota/personaji/dota2/dao/AbilityRepository.java
+++ src/main/java/com/dota/personaji/dota2/dao/AbilityRepository.java
@@ -4,8 +4,6 @@ import com.dota.personaji.dota2.model.Ability;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AbilityRepository extends JpaRepository<Ability, Long> {
     Ability findByName(String name);
